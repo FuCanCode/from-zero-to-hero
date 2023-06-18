e2ee85bff27585d7b60cb2d54ca28573ffbddd1b
FuCanCode <fustef@outlook.de>
2023-06-18 20:44:15 +0200

    Destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +30,30 @@ const restaurant = {
     },
   },
 };
+console.log(restaurant.order(2, 1));
+// "Classic" way of destructuring
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log('Classic: ', a, b, c);
+// ES6
+const [x, y, z] = arr; // looks like array but is just the syntax for 3 new variables
+console.log('ES6: ', x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+//Switching variables
+[main, secondary] = [secondary, main];
+
+// Receive 2 return values from a function and store immediately in two two different variables
+const [myStarter, myMain] = restaurant.order(2, 0);
+
+// Nested array
+const nested = [2, 4, [5, 6]];
+const [nest1, , [nest3, nest4]] = nested;
+console.log(nest1, nest3, nest4);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
