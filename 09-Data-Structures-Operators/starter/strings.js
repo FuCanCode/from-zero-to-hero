d9fc2dcf74563229942c6133a38fb08978e0ad4a
FuCanCode <fustef@outlook.de>
2023-06-30 16:20:49 +0200

    challenge-3

---
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[2]);
+console.log('B737'[2]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log('B737'.lastIndexOf('7'));
+console.log(airline.indexOf('Portugal'));
