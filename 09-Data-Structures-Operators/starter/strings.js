f2040e3ff5e7d2d91974847ddc2fd665d9d98d0e
FuCanCode <fustef@outlook.de>
2023-06-30 22:34:58 +0200

    Strings 1

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -11,3 +11,18 @@ console.log('B737'.length);
 console.log(airline.indexOf('r'));
 console.log('B737'.lastIndexOf('7'));
 console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 10)); // stops extracting BEFORE 10
+console.log(airline.slice(0, airline.indexOf(' '))); // First word
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // Last word
+
+console.log(airline.slice(-3)); // will start extracting from last three indexes
+console.log(airline.slice(1, -1));
+
+function getSeatArea(seat) {
+  const area =
+    'BE'.indexOf(seat.slice(-1)) !== -1 ? 'middle' : 'window or aisle';
+  console.log(`You've got a ${area} seat.`);
+}
+getSeatArea('11A');
