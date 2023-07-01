9ec449692ca0d41f06299ed78aad3172ebd09592
FuCanCode <fustef@outlook.de>
2023-07-01 11:47:12 +0200

    Strings 2

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const airline = 'TAP Air Portugal';
+///// Index and slice
+/*const airline = 'TAP Air Portugal';
 const plane = 'A320';
 console.log(plane[2]);
 console.log('B737'[2]);
@@ -25,4 +26,87 @@ function getSeatArea(seat) {
     'BE'.indexOf(seat.slice(-1)) !== -1 ? 'middle' : 'window or aisle';
   console.log(`You've got a ${area} seat.`);
 }
-getSeatArea('11A');
+getSeatArea('11A'); */
+
+///// Cases
+/* console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'sTeFAn';
+const passengerLower = passenger.toLowerCase();
+const passengerFixed =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerFixed);
+
+const fixName = function (unfixed) {
+  return (
+    unfixed.toLowerCase()[0].toUpperCase() + unfixed.toLowerCase().slice(1)
+  );
+};
+console.log(fixName('sIGurD'));
+
+// Comparing emails
+const email = 'fufu@code.it';
+const loginEmail = ' FuFu@Code.iT \n';
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail === email);
+
+const normalizedEmail = loginEmail.trim().toLowerCase(); // Chaining methods
+console.log(normalizedEmail);
+
+// Replace
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'Dear passengers please go to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate')); // Replace all by using RegExp with "g"
+// console.log(announcement.replaceAll('door', 'gate')); // NEW method
+
+// Bool
+const plane2 = 'Airbus A320neo';
+console.log(plane2.includes('neo'));
+console.log(plane2.startsWith('Boeing'));
+
+if (plane2.startsWith('Airbus') && plane2.endsWith('neo'))
+  console.log('Part of the NEW Airbus family');
+
+// Practice exercise
+const checkBaggage = function (items) {
+  items = items.toLowerCase();
+  items.includes('bomb') || items.includes('knife')
+    ? console.log('Freeze and go to the ground!')
+    : console.log('Enjoy your flight!');
+};
+checkBaggage('I have a bomb, food, socks and shit');
+checkBaggage('I have  a pillow and some snacks.');
+checkBaggage('I have a some sex toys and a knife.'); */
+
+// Slit and join
+console.log('a+very+nice+string'.split('+')); // will create an array
+console.log('Stefan Foodslaughter'.split(' '));
+
+const [firstName, lastName] = 'Stefan Foodslaughter'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+// Practice
+const cpaitalize = function (fullName) {
+  const array = fullName.split(' ');
+  const newArray = [];
+  for (let e of array) {
+    e = e[0].toUpperCase() + e.slice(1);
+    newArray.push(e); // Could be integrated in the lline above
+  }
+  return newArray.join(' ');
+};
+
+const passenger = 'jessica ann smith davies';
+const passengerCap = cpaitalize(passenger);
+console.log(passengerCap);
