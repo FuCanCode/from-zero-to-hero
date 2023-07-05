74a9cac4efc8e68a2422f3e304cf7157fb5e4fbf
FuCanCode <fustef@outlook.de>
2023-07-05 10:53:16 +0200

    call, apply and bind method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -87,7 +87,7 @@ document.body.addEventListener('click', high5);
 ['Stefan', 'Dennis', 'Fist'].forEach(high5); */
 
 ///// Functions returning functions
-const greet = function (greeting) {
+/* const greet = function (greeting) {
   return function (name) {
     console.log(`${greeting} ${name}`);
   };
@@ -100,4 +100,91 @@ greet('Hello')('Me'); // First part is equal to the returned function which can 
 
 // As an arrow function
 const greet2 = greeting => name => console.log(`${greeting} ${name}`);
-greet2('Arrow')('hitted');
+greet2('Arrow')('hitted'); */
+
+///// The call and apply methods
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console
+      .log
+      // `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+      ();
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(778, 'Waldemar');
+lufthansa.book(666, 'Gevatter Tod');
+// console.log(lufthansa.bookings);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book; // make a copy of the object method to create a global function
+
+// The call() method
+book.call(lufthansa, 654, 'Olaf Schubert');
+// console.log(lufthansa.bookings);
+
+book.call(eurowings, 333, 'Mai Porn');
+// console.log(eurowings.bookings);
+
+// The apply() method
+const flightData = [225, 'Jack Nicholson'];
+book.apply(eurowings, flightData);
+// console.log(eurowings.bookings);
+
+// Best practice: call() method with spread operator
+book.call(lufthansa, ...flightData);
+// console.log(lufthansa.bookings);
+
+///// The bind() method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+bookEW(404, 'Captain Lost');
+// console.log(eurowings.bookings);
+
+// creating "presets" for functions with the bind() method
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Tina Turner');
+// console.log(eurowings.bookings);
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+console.log(lufthansa);
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // bind() needed, otherwise "this" pointing to the caller of the function (the Button)
+
+// Partial application (preset parameters)
+const addTax = (rate, value) => value + (value * rate) / 100;
+console.log(addTax(10, 200));
+
+const addVAT23 = addTax.bind(null, 23); // it's a convention to use "null" if we don't care about "this"
+console.log(addVAT23(300));
+
+// Challenge returning function
+const taxUs = value => rate => console.log(value + (value * rate) / 100); // my solution
+taxUs(100)(15);
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + (value * rate) / 100;
+  };
+};
+const addVAT = addTaxRate(45);
+console.log(addVAT(100));
+console.log(addVAT(88));
+console.log(addTaxRate(19)(200));
