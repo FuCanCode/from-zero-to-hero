d3c5f376695c453ae1bf10bd3c1353ae11616d85
FuCanCode <fustef@outlook.de>
2023-07-20 08:02:06 +0200

    practice arrays

--- 11-Arrays-Bankist/starter/practiseArrays.js
+++ 11-Arrays-Bankist/starter/practiseArrays.js
@@ -31,7 +31,6 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 ///// 1. Task: Get the sum of all deposits
-
 const bankDepositSum = accounts
   .flatMap(acc => acc.movements)
   .filter(mov => mov > 0)
@@ -43,7 +42,6 @@ console.log(
 );
 
 ///// 2. Count deposits with at least 1000
-
 const depGT1000 = accounts
   .flatMap(acc => acc.movements)
   //   .filter(dep => dep >= 1000)
@@ -56,3 +54,50 @@ console.log(
   `Number of deposits greater than 1000: %c${depGT1000}`,
   'color: green'
 );
+
+///// 3. create new Object that contains sum of deposits and withdrawals
+const sumsObj = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(sumsObj);
+
+///// 4. Convert to title case (first letter capitalized with exceptions like "a", "the"...)
+const exmpl = 'the stupid cat jumps over a dumb dog';
+const exmpl2 = 'another BIG sentence WiTh A bad WOrding';
+const exceptions = ['in', 'the', 'a', 'on', 'and', 'is', 'with'];
+
+const convertTitlecase = function (sentence) {
+  // divide sentence
+  const arr = sentence.split(' ');
+  // capitalize first letter with a condition
+  return arr.reduce((sentence, word) => {
+    return exceptions.includes(word)
+      ? (sentence += ' ' + word)
+      : (sentence += ' ' + word[0].toUpperCase() + word.slice(1));
+  }, '');
+};
+
+console.log(convertTitlecase(exmpl));
+
+const convertTitlecase2 = function (sentence = 'placeholder') {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  return capitalize(
+    sentence
+      .toLowerCase()
+      .split(' ')
+      .map(word =>
+        exceptions.includes(word) ? word.toLowerCase() : capitalize(word)
+      )
+      .join(' ')
+  );
+};
+
+console.log(convertTitlecase2(exmpl));
+console.log(convertTitlecase2(exmpl2));
