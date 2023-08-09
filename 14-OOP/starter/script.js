14029c7026659bf80356131fcfca1330fd23c6f8
FuCanCode <fustef@outlook.de>
2023-08-09 12:16:22 +0200

    constructor functions and "new" operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//ANCHOR - 208. Constructor functions and the <new> operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create methods in constructor function!
+  // Will cause performance issues when dealing with many instances of the object
+  this.calcAge = function () {
+    return new Date().getFullYear() - this.birthYear;
+  };
+  this.age = this.calcAge();
+};
+
+const futt = new Person('Futterhacker', 1986);
+console.log(futt);
+
+// 1. New {} (Obj) is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. constructor function automatically return {}
+
+const mai = new Person('Mai', 1996);
+console.log(mai, mai instanceof Person);
+console.log(mai.calcAge());
