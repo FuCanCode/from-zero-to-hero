8e61656020fce14406e8dee444abb390f037d7c5
FuCanCode <fustef@outlook.de>
2023-08-10 11:00:58 +0200

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -8,10 +8,10 @@ const Person = function (firstName, birthYear) {
 
   // Never create methods in constructor function!
   // Will cause performance issues when dealing with many instances of the object
-  this.calcAge = function () {
-    return new Date().getFullYear() - this.birthYear;
-  };
-  this.age = this.calcAge();
+  // this.calcAge = function () {
+  // return new Date().getFullYear() - this.birthYear;
+  // };
+  // this.age = this.prototype.calcAge();
 };
 
 const futt = new Person('Futterhacker', 1986);
@@ -24,4 +24,23 @@ console.log(futt);
 
 const mai = new Person('Mai', 1996);
 console.log(mai, mai instanceof Person);
+
+//ANCHOR - 209. Prototypes
+console.log(Array.prototype);
+
+Person.prototype.calcAge = function () {
+  return new Date().getFullYear() - this.birthYear;
+};
 console.log(mai.calcAge());
+
+console.log(mai.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(mai)); // true
+console.log(Array.prototype.isPrototypeOf([1, 2, 3])); // true
+console.log(String.prototype.isPrototypeOf('No!')); // false, because it's primitive
+const str = new String("I'm a sexy beast!");
+console.log(String.prototype.isPrototypeOf(str)); // true, because now an object
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(mai.species); // Homo Sapiens, but actually not "own" property
+console.log(mai.hasOwnProperty('species')); // false
+console.log(mai.hasOwnProperty('firstName')); // true
