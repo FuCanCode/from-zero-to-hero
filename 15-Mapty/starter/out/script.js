737f19358f0d31bb86d15a55366df9f2c1e4ab0b
FuCanCode <fustef@outlook.de>
2023-08-25 22:12:06 +0200

    233. Leaflet library

--- 15-Mapty/starter/out/script.js
+++ 15-Mapty/starter/out/script.js
@@ -1,4 +1,5 @@
 "use strict";
+// import * as L from 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const form = document.querySelector('.form');
@@ -8,14 +9,27 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+//ANCHOR - 232. Geolocation API
 if (navigator.geolocation) {
     const success = function (pos) {
         const { longitude } = pos.coords;
         const { latitude } = pos.coords;
         console.log(`https://www.google.de/maps/@${latitude},${longitude}`);
+        // Leaflet
+        const L = window.L;
+        const coords = [latitude, longitude];
+        const map = L.map('map').setView(coords, 15);
+        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+        L.marker(coords)
+            .addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     };
     const error = function () {
         alert('Cannot get location');
     };
     navigator.geolocation.getCurrentPosition(success, error);
 }
+//ANCHOR - 233. Map using Leaflet library
--- 15-Mapty/starter/script.ts
+++ 15-Mapty/starter/script.ts
@@ -1,3 +1,4 @@
+// import * as L from 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -9,23 +10,36 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// const success = function (pos) {
-//   console.log(pos.coords);
-
-interface Coords {
-  x: number;
-  y: number;
-}
-
+//ANCHOR - 232. Geolocation API
 if (navigator.geolocation) {
   const success = function (pos: GeolocationPosition) {
     const { longitude } = pos.coords;
     const { latitude } = pos.coords;
     console.log(`https://www.google.de/maps/@${latitude},${longitude}`);
+
+    // Leaflet
+    const L: any = window.L;
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 15);
+
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }
+    ).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   };
 
-  const error = function () {
+  const error = function (): void {
     alert('Cannot get location');
   };
   navigator.geolocation.getCurrentPosition(success, error);
 }
+
+//ANCHOR - 233. Map using Leaflet library
