f31a7fd71a0a49497e91b0a12231238cc4d5a837
Stefan F <fustef@outlook.de>
2023-10-04 18:05:57 +0200

    274 the module pattern

--- 17-Modern-JS-Modules-Tooling/starter/dist/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/dist/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /* import {
   addToCart,
   cart,
@@ -20,18 +21,51 @@ console.log(...cart, price, q, ShoppingCart.shippingCost);
 anyName('Bier', 12);
 
 */
-import addToCart from './shoppingCart.js';
-addToCart('Flutschi', 8);
-////// Top-level await
-console.time('x');
-const whyNot = fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json());
-console.log(whyNot);
-const waited = await whyNot;
-console.log(waited);
-console.timeEnd('x');
-const getLastPost = async function () {
-    const stream = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await stream.json();
-    return { title: posts.at(-1)?.title, text: posts.at(-1)?.body };
-};
-console.log((await getLastPost()).text);
+// import addToCart from './shoppingCart.js';
+// addToCart('Flutschi', 8);
+// ////// Top-level await
+// console.time('x');
+// const whyNot = fetch('https://jsonplaceholder.typicode.com/todos').then(
+//   response => response.json()
+// );
+// console.log(whyNot);
+// const waited = await whyNot;
+// console.log(waited);
+// console.timeEnd('x');
+// interface IPost {
+//   id: number;
+//   userId: number;
+//   title: string;
+//   body: string;
+//   completeted: boolean;
+// }
+// const getLastPost = async function () {
+//   const stream = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const posts: IPost[] = await stream.json();
+//   return { title: posts.at(-1)?.title, text: posts.at(-1)?.body };
+// };
+// console.log((await getLastPost()).text);
+///////////////////////////////////
+/// 274. The Module Pattern
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 465;
+    const totalQuantity = 52;
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to Cart.`);
+    };
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+ShoppingCart2.addToCart('Bier', 5);
+ShoppingCart2.blub = 5;
+console.log(ShoppingCart2.cart, ShoppingCart2);
