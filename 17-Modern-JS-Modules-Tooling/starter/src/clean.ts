589353809c8c073deccd7cfd4677bef3f8dabbbf
Stefan F <fustef@outlook.de>
2023-10-11 11:35:21 +0200

    implemented basic FP

--- 17-Modern-JS-Modules-Tooling/starter/src/clean.ts
+++ 17-Modern-JS-Modules-Tooling/starter/src/clean.ts
@@ -5,7 +5,7 @@ interface Budget {
   flag?: string;
 }
 
-const budget: Budget[] = [
+const budget: readonly Budget[] = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -14,51 +14,65 @@ const budget: Budget[] = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits: Record<string, number> = {
+const spendingLimits: Record<string, number> = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
 
-const getLimit = (user: string) => spendingLimits?.[user] ?? 0;
+const getLimit = (limits: Record<string, number>, user: string) =>
+  limits?.[user] ?? 0;
 
+// Pure function
 const addExpense = (
+  state: readonly Budget[],
+  limits: Record<string, number>,
   value: number,
   description: string,
   user: string = 'jonas'
 ) => {
-  user = user.toLowerCase();
+  const cleanUser = user.toLowerCase();
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  } else
-    console.log(
-      `User ${user}'s expense (${value} for "${description}") was rejected because the spending limit was exceeded!`
-    );
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+console.log(newBudget1);
+
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(newBudget3);
+
+const checkBudget = (
+  state: readonly Budget[],
+  limits: Record<string, number>
+): Budget[] =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+const checkedBudget: Budget[] = checkBudget(newBudget3, spendingLimits);
+console.log('Checked: ', checkedBudget);
+
 console.log(budget);
 
-const checkBudget = () => {
-  budget.forEach(entry => {
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-  });
+const showBigExpenses = function (state: Budget[], limit: number) {
+  const bigExpenses = state.filter(e => e.value <= -limit);
+  return bigExpenses
+    .reduce((icons, entry) => {
+      return (icons += `${entry.description.slice(-2)} / `);
+    }, '')
+    .slice(0, -2);
 };
-checkBudget();
-
-console.log(budget);
-
-const showBigExpenses = function (limit: number) {
-  let output = '';
-  for (const entry of budget) {
-    entry.value <= -limit
-      ? (output += entry.description.slice(-2) + ' / ')
-      : output; // Emojis are 2 chars
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-showBigExpenses(100);
+const bigExpensesSymbols = showBigExpenses(checkedBudget, 100);
+console.log(bigExpensesSymbols);
