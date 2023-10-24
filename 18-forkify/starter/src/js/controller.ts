6dc3e5d4bb41cd4c456a06902882b959b72f390c
Stefan F <fustef@outlook.de>
2023-10-24 18:59:00 +0200

    adjusted icons path for parcel

--- 18-forkify/starter/src/js/controller.ts
+++ 18-forkify/starter/src/js/controller.ts
@@ -1,4 +1,5 @@
 import { Ingredients, RecipeBase, RecipeDetails } from './types';
+import icons from '../img/icons.svg';
 
 const recipeContainer = document.querySelector('.recipe') as HTMLDivElement;
 
@@ -50,7 +51,7 @@ const showRecipe = async function (id: string): Promise<RecipeDetails> {
           html +
           `<li class="recipe__ingredient">
       <svg class="recipe__icon">
-        <use href="src/img/icons.svg#icon-check"></use>
+        <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${ing.quantity}</div>
       <div class="recipe__description">
@@ -73,34 +74,27 @@ const showRecipe = async function (id: string): Promise<RecipeDetails> {
   <div class="recipe__details">
     <div class="recipe__info">
       <svg class="recipe__info-icon">
-        <use href="${new URL(
-          '../img/icons.svg#icon-clock',
-          import.meta.url
-        )}"></use>
+        <use href="${icons}#icon-clock"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${
-        recipe.cookingTime
-      }</span>
+      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
       <span class="recipe__info-text">minutes</span>
     </div>
     <div class="recipe__info">
       <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-users"></use>
+        <use href="${icons}#icon-users"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        recipe.servings
-      }</span>
+      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
       <span class="recipe__info-text">servings</span>
 
       <div class="recipe__info-buttons">
         <button class="btn--tiny btn--increase-servings">
           <svg>
-            <use href="src/img/icons.svg#icon-minus-circle"></use>
+            <use href="${icons}#icon-minus-circle"></use>
           </svg>
         </button>
         <button class="btn--tiny btn--increase-servings">
           <svg>
-            <use href="src/img/icons.svg#icon-plus-circle"></use>
+            <use href="${icons}#icon-plus-circle"></use>
           </svg>
         </button>
       </div>
@@ -108,12 +102,12 @@ const showRecipe = async function (id: string): Promise<RecipeDetails> {
 
     <div class="recipe__user-generated">
       <svg>
-        <use href="src/img/icons.svg#icon-user"></use>
+        <use href="${icons}#icon-user"></use>
       </svg>
     </div>
     <button class="btn--round">
       <svg class="">
-        <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+        <use href="${icons}#icon-bookmark-fill"></use>
       </svg>
     </button>
   </div>
@@ -129,9 +123,7 @@ const showRecipe = async function (id: string): Promise<RecipeDetails> {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipe.publisher
-            }</span>. Please check out
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
             directions at their website.
           </p>
           <a
@@ -141,7 +133,7 @@ const showRecipe = async function (id: string): Promise<RecipeDetails> {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>
